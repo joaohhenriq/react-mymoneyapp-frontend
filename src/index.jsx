18cd6c03da3f6f4358d772453e84e0825d58d02c
joaoh <joao_camargo10@hotmail.com>
2019-12-29 09:58:38 -0300

    Dashboard redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import App from './main/app'
+import reducers from './main/reducers'
+import promise from 'redux-promise'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const store = applyMiddleware(promise)(createStore)(reducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+)
