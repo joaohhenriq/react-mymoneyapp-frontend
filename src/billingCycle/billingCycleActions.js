5eef0dd29a92587a1a79e221c591a1e33bb69187
joaoh <joao_camargo10@hotmail.com>
2019-12-30 09:28:33 -0300

    Middlewares (multi and thunk) to call other actions from current action

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+
+import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3004/api'
 
@@ -12,14 +15,22 @@ export function getList() {
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
-        .then(resp => {
-            toastr.success('Sucesso', 'Operação realizada com sucesso.')
-        })
-        .catch(e => {
-            e.response.data.errors.forEach(error => toastr.error('Erro', error))
-        })
-    return {
-        type: 'TEMP'
+    //redux thunk: allows an action calls another action(s)
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação realizada com sucesso.')
+                //redux multi: allows use dispatch like an array to pass the actions that will be
+                //called atfer the current action
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate')
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            })
     }
 }
