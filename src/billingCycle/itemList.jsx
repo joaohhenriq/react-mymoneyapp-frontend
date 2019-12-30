dcd44a12f608d1d6808ac23f6c52155af5ff06bb
joaoh <joao_camargo10@hotmail.com>
2019-12-30 18:27:10 -0300

    Upgrading component to be reused

--- src/billingCycle/itemList.jsx
+++ src/billingCycle/itemList.jsx
@@ -6,17 +6,17 @@ import { Field, arrayInsert, arrayRemove } from 'redux-form'
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
 
-class CreditList extends Component {
+class ItemList extends Component {
 
     renderRows() {
         const list = this.props.list || []
         return list.map((item, index) => (
             <tr key={index}>
-                <td><Field name={`credits[${index}].name`}
+                <td><Field name={`${this.props.field}[${index}].name`}
                     placeholder='Informe o nome'
                     readOnly={this.props.readOnly}
                     component={Input} /></td>
-                <td><Field name={`credits[${index}].value`}
+                <td><Field name={`${this.props.field}[${index}].value`}
                     placeholder='Informe o valor'
                     readOnly={this.props.readOnly}
                     component={Input} /></td>
@@ -40,13 +40,13 @@ class CreditList extends Component {
 
     add(index, item = {}) {
         if (!this.props.readOnly) {
-            this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+            this.props.arrayInsert('billingCycleForm', this.props.field, index, item)
         }
     }
 
     remove(index) {
         if (!this.props.readOnly && this.props.list.length > 1) {
-            this.props.arrayRemove('billingCycleForm', 'credits', index)
+            this.props.arrayRemove('billingCycleForm', this.props.field, index)
         }
     }
 
@@ -54,7 +54,7 @@ class CreditList extends Component {
         return (
             <Grid cols={this.props.cols}>
                 <fieldset>
-                    <legend>Créditos</legend>
+                    <legend>{this.props.legend}</legend>
                     <table className='table'>
                         <thead>
                             <tr>
@@ -74,4 +74,4 @@ class CreditList extends Component {
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert, arrayRemove }, dispatch)
-export default connect(null, mapDispatchToProps)(CreditList)
+export default connect(null, mapDispatchToProps)(ItemList)
